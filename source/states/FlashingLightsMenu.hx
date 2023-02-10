d34e6e4a09fa91f32ce160d4174d0fccab22a5e0
Leather128 <81660872+Leather128@users.noreply.github.com>
2023-02-10 18:43:35 -0500

    better flashing lights menu

--- source/states/FlashingLightsMenu.hx
+++ source/states/FlashingLightsMenu.hx
@@ -1,33 +1,41 @@
 package states;
 
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
 import utilities.Options;
 import flixel.FlxG;
 import flixel.util.FlxColor;
 import flixel.text.FlxText;
 
-class FlashingLightsMenu extends MusicBeatState
-{
-    override public function create()
-    {
-        super.create();
+class FlashingLightsMenu extends MusicBeatState {
+	var text:FlxText;
 
-        var text = new FlxText(0,0,0,"Hey! Leather Engine has flashing lights\nPress Y to enable them, or anything else to not.\n(Any key closes this menu)", 32);
-        text.font = Paths.font("vcr.ttf");
-        text.screenCenter();
-        text.setBorderStyle(OUTLINE, FlxColor.BLACK, 1.5, 1);
-        add(text);
-    }
+	override public function create() {
+		super.create();
 
-    override function update(elapsed:Float)
-    {
-        super.update(elapsed);
+		text = new FlxText(0, 0, 0, "Hey! Leather Engine has flashing lights\nPress Y to enable them, or anything else to not.\n(Any key closes this menu)",
+			32);
+		text.font = Paths.font("vcr.ttf");
+		text.screenCenter();
+		text.setBorderStyle(OUTLINE, FlxColor.BLACK, 1.5, 1);
+		add(text);
+	}
 
-        if(FlxG.keys.justPressed.Y)
-            Options.setData(true, "flashingLights");
-        else if(!FlxG.keys.justPressed.Y && FlxG.keys.justPressed.ANY)
-            Options.setData(false, "flashingLights");
+	override function update(elapsed:Float) {
+		super.update(elapsed);
 
-        if(FlxG.keys.justPressed.ANY)
-            FlxG.switchState(new TitleState());
-    }
+		if (FlxG.keys.justPressed.Y)
+			Options.setData(true, "flashingLights");
+		else if (!FlxG.keys.justPressed.Y && FlxG.keys.justPressed.ANY)
+			Options.setData(false, "flashingLights");
+
+		if (FlxG.keys.justPressed.ANY) {
+			FlxG.sound.play(Paths.sound("confirmMenu"));
+
+			FlxTween.tween(text, {alpha: 0}, 2.5, {
+				ease: FlxEase.cubeInOut,
+				onComplete: (_) -> FlxG.switchState(new TitleState())
+			});
+		}
+	}
 }
