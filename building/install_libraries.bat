63c1b8ef06a0761a30cd140332eb00399e85fdcf
Vortex <73261680+Vortex2Oblivion@users.noreply.github.com>
2023-05-29 15:53:32 +0000

    Update install_libraries.bat

--- building/install_libraries.bat
+++ building/install_libraries.bat
@@ -6,3 +6,4 @@ haxelib install hscript
 haxelib install polymod 1.7.0
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib git linc_luajit https://github.com/Leather128/linc_luajit.git
+haxelib git fnf-modcharting-tools https://github.com/TheZoroForce240/FNF-Modcharting-Tools
