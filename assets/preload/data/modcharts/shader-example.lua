0b60512150e823dad9484e714957f79fd300c432
Vortex <73261680+Vortex2Oblivion@users.noreply.github.com>
2023-07-12 12:11:22 +0000

    Update shader-example.lua

--- assets/preload/data/modcharts/shader-example.lua
+++ assets/preload/data/modcharts/shader-example.lua
@@ -1,13 +1,18 @@
 function start( song )
-    --                    id    filename  camera
-    createCustomShader("example", "example", 120)
+    print(song)
+    --                    id    filename  glsl ver
+    createCustomShader("example", "example", 120) -- creating the shader
     --                       id      camera
-    setCameraCustomShader("example", "game")
-    setCameraCustomShader("example", "hud")
+    setCameraCustomShader("example", "game") -- setting shader to camGame
+    setCameraCustomShader("example", "hud") -- setting shader to camHUD
 end
 function beatHit( beat )
-    if curStep == 25 then -- turning off shaders after 25 beats
+    if curBeat == 25 then -- turning off shaders after 25 beats
         setCameraNoCustomShader("hud")
         setCameraNoCustomShader("game")
+    end
+    if curBeat == 30 then -- changing the icons to have a shader after 30 beats
+        setActorCustomShader("example", "iconP1")
+        setActorCustomShader("example", "iconP2")
     end
 end
