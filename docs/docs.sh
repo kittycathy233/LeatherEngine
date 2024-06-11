67b6579a6fd6496ee39c2552c30a17779350c5a5
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2024-06-10 21:14:21 -0400

    one last try

--- docs/docs.sh
+++ docs/docs.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 haxe docs/docs.hxml
-haxelib run dox -i docs -o pages --title "Leather Engine Documentation" -ex .*^ -in /* 
+haxelib run dox -i docs -o pages --title "Leather Engine Documentation" -in /*
