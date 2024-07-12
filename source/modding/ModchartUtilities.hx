20176ec2044dc89afe98d0145455c54c9ee1621f
Vortex <73261680+Vortex2Oblivion@users.noreply.github.com>
2024-07-12 23:19:07 +0000

    add fastTan to lua

--- source/modding/ModchartUtilities.hx
+++ source/modding/ModchartUtilities.hx
@@ -3200,6 +3200,10 @@ setLuaFunction("setActor3DShader", function(id:String, ?speed:Float = 3, ?freque
 			return FlxMath.fastSin(n);
 		});
 
+		setLuaFunction("fastTan", function(n:Float) {
+			return FlxMath.fastSin(n) / FlxMath.fastCos(n0;
+		});
+
 		setLuaFunction("fceil", function(n:Float) {
 			return Math.fceil(n);
 		});
