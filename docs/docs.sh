9e97ee6ed3fb5b2198ae796a8e1821469b62b705
Vortex <73261680+Vortex2Oblivion@users.noreply.github.com>
2025-03-02 10:06:26 -0500

    Update docs.sh

--- docs/docs.sh
+++ docs/docs.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 haxe docs/docs.hxml
-haxelib run dox -i docs -o pages --title "Leather Engine Documentation"
+haxelib run dox -i docs -o pages --title "Leather Engine Documentation" -D source-path https://github.com/Vortex2Oblivion/LeatherEngine/tree/main/source
