7640371fcc94f8aaabd7897cb6de8689f7649a9e
Vortex <73261680+Vortex2Oblivion@users.noreply.github.com>
2025-03-02 09:56:59 -0500

    Update docs.bat

--- docs/docs.bat
+++ docs/docs.bat
@@ -1,3 +1,3 @@
 
 haxe docs/docs.hxml
-haxelib run dox -i docs -o pages --title "Leather Engine Documentation"
+haxelib run dox -i docs -o pages --title "Leather Engine Documentation" -D source-path https://github.com/Vortex2Oblivion/LeatherEngine/tree/main/source
