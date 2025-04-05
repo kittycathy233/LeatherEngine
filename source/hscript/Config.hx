8d8c9752455462c75e8bd9035196497efe6ac7b9
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2025-04-04 20:15:37 -0400

    allow for game classes to be extended with hscript

---
+++ source/hscript/Config.hx
@@ -0,0 +1,33 @@
+package hscript;
+
+@:dox(hide)
+class Config {
+	// Runs support for custom classes in these
+	public static final ALLOWED_CUSTOM_CLASSES = [
+		#if !dox
+		"flixel",
+        "game",
+		"modding",
+		"states",
+		//"shaders",
+		"substates",
+		"ui"
+		#end
+	];
+
+	// Runs support for abstract support in these
+	public static final ALLOWED_ABSTRACT_AND_ENUM = [
+		#if !dox
+		"flixel",
+		"openfl.display.BlendMode",
+		#end
+	];
+
+	// Incase any of your files fail
+	// These are the module names
+	public static final DISALLOW_CUSTOM_CLASSES = [
+	];
+
+	public static final DISALLOW_ABSTRACT_AND_ENUM = [
+	];
+}
